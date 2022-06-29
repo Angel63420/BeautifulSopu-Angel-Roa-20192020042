1a70a0c2e4f90ab3d820e18a2c037a7a2e0f13f5
Angel63420 <95395627+Angel63420@users.noreply.github.com>
2022-06-29 10:57:19 -0500

    Add files via upload

--- ScrapingCondor/main.py
+++ ScrapingCondor/main.py
@@ -1,50 +1,46 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from itertools import count
-
+# Librerias utilizadas
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
 import time
-import winsound
 
-def buscaMateria():
-    #Física II
-    url = 'https://estudiantes.portaloas.udistrital.edu.co/academicopro/index.php?index=g8e-WrLHvTv5BT8Sn-33RtAntsWNA9F6JHNBNeJb34cPogK5CLDRyAt6oDq2o6TvXR3hYPJhnCFhf-ym3CII_Yj0APuoUbK_rSKZyGhNMgn4iljw53PQMnSmMs3Y2iiLzDO2Jz6UXIYKmdPgp8BVybiUGdJidmXATa1uyedjo2hYKCc6P21g06ETSXEMlBwQeZ2Kc_l8HeR-SPTXAb254DzuRZ2h97QC367Y0pujXiFbwD6CjlTNMd6_k_9ja1E4uGxX6y-sZq5cRmy0pwJQnUxJHZ-fjyy96HPUD9679sXuTsyja_bItwcBM5xsmtUSjf4LHlN4ogKbJvv5Mcg--BGx5l1_K5S2qhyu4bmqLnSG4L0GUHdpB1PILpvQxhy1yRIC-eYEMW9di49YsUEjoZX2CNBktNJWNWPuCURBYHfF9yq33V2sHIp8pZoL8V-v5GD4-2WXOtm2uHywfov0Y4uyUvi_-VnptdqfZV4-YaYJ8a9PCyNWW_A_5qx4DyYUlLR7j0-8O-d3eytgYnUFs0RZktUwOG2KFGj3q5noU45h6hM5QuiCT6nSIDtW3bhqBLrqgzwDAWZX2nD1IZY5Lg'
+#La libreria windsound se usa para emitir el sonido en Linux 
+from playsound import playsound
+#La libreria windsound se usa para emitir el sonido en windows 
+#import winsound
+
+#Funcion encargada de localizar y monitorizar nuestro punto objetivo
+
+def buscaCita():
+  
+    #Url de la pagina a monitorizar
+    url = 'ingrese aqui la Url de la pagina a monitorear sin quitar las comillas'
+
+    #Crea la sopa y obtiene el contenido de la pagina
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    td = soup.find_all('button', class_='botonEnlacePreinscripcion')
-
+    #Encuentra el boton dentro del contenido extraido y lo guarda
+    td = soup.find_all('button', class_='ingrese aqui el nombre de la clase del boton a monitorear sin quitar las comillas')
+  
+    #Contador que nos indica si el boton que buscamos esta activo
     count = 0
     for i in td:
         count += 1
 
+    #If que nos avisa cuando ha encontrado la cita
     if(count > 0):
-        print(str(count) + ' Física II')
-        winsound.PlaySound("Silbido", winsound.SND_FILENAME)
+        print(str(count) + ' Cita encontrada')
+        #Dependiendo de si estamos en Linux o en Windows debemos activar un reproductor de sonido u otro, Playsound en e caso de Linux y Windsound en el caso de windwos
+        playsound('Silbido.mp3')
+         #winsound.PlaySound("Silbido", winsound.SND_FILENAME)
 
-    # Fundamentos
-    url = 'https://estudiantes.portaloas.udistrital.edu.co/academicopro/index.php?index=g8e-WrLHvTv5BT8Sn-33RtAntsWNA9F6JHNBNeJb34dJkptDwjeFSeFgp-O9WbdQt4fwV4hJ9SUizWuLqZ0cbkoPxD-o6ksCn1cY7sjfBcdsrNqTagFIoFXYc37r-FHk_8Beuksm-Cpit8hGtelzbndF1hGjjRanFsj5emC66I4ixwv3LjBzGvtjqNirgbYyUflXoHze_fYpS8QZyKoj46k107A-XdhbVSzZhndUY7XQjdoIzMYVNB4Q4amDs0ZwpOTZQM2lRKwBesLaPJ89ogAqpJZWc0eoWqACErPcT2vPmXJmZeJweq3gT9AtxN0WeNgs054wrHsIn__yhmK8eLDVZI6miGeB2_tP5zZtkAdHTIWPMYKqvIQhh1CqAEbqs9aowJ9XuRb_9-jun0SaPk5kJhFoFi3yY8v6ZBuAN9i4s9F8i4gMz0KsgfpMtS1XQOJHyHTTCt2B6g0QVKCkRui1KLEgerpIRb6ULsP9cKG0wMcTSloiwC74NEiD_vrLr0W1rOxmgqFe53Yp3JPGJoUjoromahNlSEPlFKa2ImNaP-Toi1ODi-ood3B0HObs6J7FeKq7b1-gS9HifM2GtA'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    #Tiempo de descanso hasta la proxima busqueda
+    time.sleep(1)
 
-    td = soup.find_all('button', class_='botonEnlacePreinscripcion')
-
-    count = 0
-    for i in td:
-        count += 1
-
-    if (count > 0):
-        print(str(count) + ' Fundamentos')
-        winsound.PlaySound("Silbido", winsound.SND_FILENAME)
-
-    time.sleep(10)
-
+#Contador de cuantas veces se ha realizado la busqueda
 contadorDeBusquedas = 0;
 while True:
-    buscaMateria()
+    buscaCita()
     contadorDeBusquedas += 1
     print(str(contadorDeBusquedas) + ' veces buscadas')
